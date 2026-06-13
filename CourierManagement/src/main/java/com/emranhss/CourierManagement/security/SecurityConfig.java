501e991e41b0b1e360c2319d8d6547d2040002f2
Muhammad Emran Hossain <emranhss@gmail.com>
2026-06-13 18:11:46 +0600

    add

---
+++ CourierManagement/src/main/java/com/emranhss/CourierManagement/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.emranhss.CourierManagement.security;
+
+
+import com.emranhss.CourierManagement.serviceimp.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity          // enables @PreAuthorize on controllers
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthFilter            jwtAuthFilter;
+    private final CustomUserDetailsService userDetailsService;
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
